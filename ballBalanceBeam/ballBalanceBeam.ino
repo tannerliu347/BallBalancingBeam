14e9c6473e7a737a5e74dbb17a38e3ab970a1555
Tanner <tliu347@gatech.edu>
2019-11-11 16:56:59 -0500

    Fixed error function & units

--- ballBalanceBeam/ballBalanceBeam.ino
+++ ballBalanceBeam/ballBalanceBeam.ino
@@ -1,15 +1,16 @@
 #include <Servo.h>
 
+// 85 degree is zero; 150 max; 30 min 
 // turn off print for better performance when not debugging
-bool debug = false;
+bool debug = true;
 
 // Gain values
-uint8_t Kp = 1;
-uint8_t Kd = 1;
+uint8_t Kp = 10;
+uint8_t Kd = 10;
 uint8_t Ki = 1;
 
 // Reference distance cm
-uint8_t reference = 10;
+uint8_t reference = 30;
 
 // timing related
 unsigned long lastMs;
@@ -26,7 +27,9 @@ float distanceRate; // cm/ms
 
 // Servo pin and variable setup
 Servo servo;
-int servoAngle;
+uint8_t servoAngle;
+uint8_t servoOffset = 85;
+
 
 
 
@@ -48,8 +51,26 @@ float distanceRead() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
   long duration = pulseIn(echoPin, HIGH);
-  return duration * 0.5 * 29.1;
+  //delay(1000);
+  return duration * 0.5 * 0.000343;
+}
+
+uint8_t pControl() {
+  return (int) Kp * distanceError + servoOffset;
+}
+
+uint8_t pdControl() {
+  return (int) Kp * (distanceError + distanceRate * Kd) + servoOffset;
+}
+
+uint8_t piControl() {
+  return 1;
+}
+
+uint8_t pidControl() {
+  return 1;
 }
 
 void loop() {
@@ -60,21 +81,25 @@ void loop() {
   lastMs = Ms;
   // read distance
   distance = distanceRead();
-  distanceError = distance - reference;
-  distanceRate = (distance - lastDistance) * 0.1; // rate of change in distance cm/ms
+  // check sensor reading is within reasonable range
+  while (distance > 90 || distance < 0) {
+    distance = distanceRead();
+  }
+  distanceError = reference - distance;
+  distanceRate = (lastDistance - distance) * 100; // rate of change in distance m/s
   lastDistance = distance;
-  // pd control
-  servoAngle = (int) Kp * (distanceError + distanceRate * Kd);
+  servoAngle = pdControl();
   // deal with saturation
-  if (servoAngle < 0) {
-    servoAngle = 0;
-  } else if (servoAngle > 165) {
-    servoAngle = 165;
+  if (servoAngle < 30) {
+    servoAngle = 30;
+  } else if (servoAngle > 150) {
+    servoAngle = 150;
   }
   servo.write(servoAngle);
   // debug print info
   if (debug) {
     Serial.print("Distance: "); Serial.print(distance); Serial.println(" cm");
+    Serial.print("distance error:"); Serial.println(distanceError); 
     Serial.print("servoAngle: "); Serial.println(servoAngle);
   }
 }
